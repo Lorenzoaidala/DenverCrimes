e270de11217e4eb51eab39c133de2cb3f2a2f0b7
Lorenzo Aidala <Lorenzo Aidala@DESKTOP-21723CJ>
2021-06-07 17:34:02 +0200

    --

--- src/main/java/it/polito/tdp/crimes/model/Model.java
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -1,6 +1,26 @@
 package it.polito.tdp.crimes.model;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.crimes.db.EventsDao;
 
 public class Model {
 	
+	private Graph<String,DefaultWeightedEdge> grafo;
+	private EventsDao dao;
+	
+	public Model() {
+		dao = new EventsDao();
+	}
+	
+	public void creaGrafo(String categoria, int mese) {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		Graphs.addAllVertices(this.grafo, dao.getVertici(categoria, mese));
+		
+			
+	}
 }
